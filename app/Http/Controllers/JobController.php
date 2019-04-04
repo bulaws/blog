680159d3fa2f82b290065c01f6431053c6ffdb2c
Tarambulavs <tarambulavs@gmail.com>
2019-04-04 17:58:41 +0300

    Add first job and run to work

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendEmailJob;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        SendEmailJob::dispatch('My mail text')->delay(now()->addSeconds(60));
+    }
+}
---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    protected $message;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $message
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        info($this->message);
+    }
+}
